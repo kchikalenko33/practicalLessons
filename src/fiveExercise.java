ea0620d41da9743aba90fe557578bddaaa2b6731
kchikalenko33 <kchikalenko33@gmail.com>
2025-10-05 07:54:15 +0300

    added five exercise

---
+++ src/fiveExercise.java
@@ -0,0 +1,16 @@
+public class fiveExercise {
+    static void main(String[] args) {
+        int ageMen = 15;
+        var ageWomen = 18;
+
+        if (ageMen > ageWomen) {
+            System.out.println("Девушка младше парня");
+        }
+        else if (ageMen == ageWomen) {
+            System.out.println("Девушка и парень - одногодки");
+        }
+        else {
+            System.out.println("Девушка старше парня");
+        }
+    }
+}
