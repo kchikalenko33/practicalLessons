fd8ccc8d26e9043cbae3237a970da746377658c4
kchikalenko33 <kchikalenko33@gmail.com>
2025-10-05 11:14:19 +0300

    Task - completed

---
+++ src/TaskOne.java
@@ -0,0 +1,19 @@
+public class TaskOne {
+//    Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+//
+//    Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+    static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 0; i <= 999; i++){
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+                sum = sum + i;
+            }
+        }
+        count = count - 1;
+        System.out.println(sum);
+        System.out.println(count);
+    }
+}
